407a4c4cd2d8aa345e9c14ff22ef62ab160bfffe
izazakuro <izazakuro@gmail.com>
2025-03-14 12:40:16 +0900

    backend set up

---
+++ go-movies-back-end/cmd/api/handlers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Go Movies up and running",
+		Version: "1.0.0",
+	}
+
+	resp, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+
+}
